87b9acbb742680b3e8a86961e5b8b1c94f9ef6b4
evillgenius75 <evill_genius@outlook.com>
2017-10-10 08:10:32 -0400

    templated script

--- challenges/SolutionHelperFiles/ch1/challenge1.sh
+++ challenges/SolutionHelperFiles/ch1/challenge1.sh
@@ -3,16 +3,16 @@
 # Create a service principal if you haven't done so already:
 # az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/mySubscriptionID"
 
-RG_NAME='k8s-1495' #Use the RG assigned to you at login of Sponsored Account
-ACS_NAME='ev2k8satl'
+RG_NAME='odl_' #Use the RG assigned to you at login of Sponsored Account
+ACS_NAME='xxxk8satl' #Change the xxx to your initials
 SSH_KEY_PATH='~/.ssh/id_rsa.pub'
 MASTER_OS_DISK=50
 AGENT_OS_DISK=120
-SP_NAME='https://hol_user_sp_1495' # Use Service Prinicpal assigned in welcome email
-SP_SECRET='xbon34GGV*tT' # Use Service Prinicpal client secret assigned in welcome email
-DNS_PREFIX='ev2atlk8s'
+SP_NAME=' https://odl_user_sp_xyz' # Use Service Prinicpal name assigned in welcome email
+SP_SECRET='xxxxxxxxxxxxx' # Use Service Prinicpal client secret assigned in welcome email
+DNS_PREFIX='xxxk8satl' #Change xxx to your initials
 VM_SIZE='Standard_D2_v2'
-ADMIN_NAME='ev2-atl'
+ADMIN_NAME='xxx-atl' #Change xxx to your initials
 
 
 az acs create --orchestrator-type Kubernetes -g $RG_NAME -n $ACS_NAME  \
